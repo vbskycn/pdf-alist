b1a283f0cd0f1f670f41fdd6141ca35dc6a40bb0
vbskycn <34542650+vbskycn@users.noreply.github.com>
2024-05-24 08:51:32 +0800

    Update viewer.js

--- web/viewer.js
+++ web/viewer.js
@@ -3419,7 +3419,7 @@ let validateFileURL;
       const fileOrigin = new URL(file, window.location.href).origin;
 
       if (fileOrigin !== viewerOrigin) {
-        throw new Error("file origin does not match viewer's");
+      //  throw new Error("file origin does not match viewer's");
       }
     } catch (ex) {
       PDFViewerApplication.l10n.get("loading_error").then(msg => {
